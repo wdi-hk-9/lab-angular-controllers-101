c1acc796c6b65c0565cfd8a4feed052501a0b2d5
Fer Martin <fernandomartinsantana@gmail.com>
2015-11-30 15:43:29 +0800

    Removed 'as' in controllers, controller function anonymous, use

--- js/donorController.js
+++ js/donorController.js
@@ -1,10 +1,8 @@
 angular.module('TheMET')
-  .controller('DonorController', DonorController);
-
-function DonorController(){
-  this.contact = {
+  .controller('DonorController', function ($scope){
+  $scope.contact = {
     email: 'development@metmuseum.org',
     phone: ' 212-650-2425'
   }
-  this.amounts = [ 50, 100, 250, 500, 1000, 5000]
-}
+  $scope.amounts = [ 50, 100, 250, 500, 1000, 5000]
+});
